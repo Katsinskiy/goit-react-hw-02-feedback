81f9d238449fb9e96fe795e5ff88158bab1a9d07
AleksandrKatsinskiy <katsinskiy@gmail.com>
2022-11-16 21:01:30 +0100

    hw react 1

---
+++ src/components/FeedbackActions/FeedbackActions.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import s from '../feedback.module.css';
+import shortid from 'shortid';
+const FeedbackActions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ul className={s.buttons}>
+      {options.map(option => (
+        <li key={shortid.generate()}>
+          <button onClick={() => onLeaveFeedback(option)} type="button">
+            {option}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FeedbackActions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string.isRequired),
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+export default FeedbackActions;
